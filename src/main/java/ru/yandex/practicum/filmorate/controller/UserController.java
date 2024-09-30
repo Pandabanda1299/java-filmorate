18464b9c5616cccdb8ddd5151e4dbbeacb2e5967
Zubarev Dmitry <dmitryshikary@yandex.ru>
2024-09-30 17:33:53 +0300

    проверка check-style 30.09)

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,8 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
-import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -22,7 +21,7 @@ public class UserController {
 
     @GetMapping
     public List<User> getUsers() {
-        LOG.info("Get all users");
+        LOG.info("Получить всех юзеров");
         return new ArrayList<>(users.values());
     }
 
@@ -31,32 +30,22 @@ public class UserController {
         validateUser(user);
         user.setId(++id);
         users.put(user.getId(), user);
-        LOG.info("User created: {}", user);
+        LOG.info("Пользователь создан: {}", user);
         return user;
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
-        if (user.getId() == null) {
-            LOG.error("Идентификатор пользователя должен быть предоставлен для обновления");
-            throw new ConditionsNotMetException("Id должен быть указан");
+        if (user.getId() != null) {
+            if (users.containsKey(user.getId())) {
+                validateUser(user);
+                users.put(user.getId(), user);
+                LOG.info("Пользователь обновлен: {}", user);
+                return user;
+            }
         }
-        User existingUser = users.get(user.getId());
-        if (existingUser == null) {
-            LOG.error("Пользователь с идентификатором  не найден", user.getId());
-            throw new ConditionsNotMetException("Пользователь не найден");
-        }
-        validateUser(user);
-        if (user.getEmail() != null && !user.getEmail().equals(existingUser.getEmail()) &&
-                users.values().stream().anyMatch(existing -> existing.getEmail().equals(user.getEmail()))) {
-            LOG.error("Адрес электронной почты уже используется", user.getEmail());
-            throw new DuplicatedDataException("Этот имейл уже используется");
-        }
-        if (user.getEmail() != null) {
-            existingUser.setEmail(user.getEmail());
-        }
-        LOG.info("Пользователь с идентификатором  обновлен.", user.getId());
-        return existingUser;
+        LOG.warn("User id is invalid");
+        throw new NotFoundException("Пользователь с id " + user.getId() + " не найден");
     }
 
     private void validateUser(User user) {
